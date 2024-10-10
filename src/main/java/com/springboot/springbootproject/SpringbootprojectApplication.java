5614ac6a769a9340aeb011756ff5f9c8bff5b2b1
abhishekgaur <abhishekgaur@localhost>
2024-10-09 22:56:48 -0700

    Code for by passing Spring security credatials page and documentation

--- src/main/java/com/springboot/springbootproject/SpringbootprojectApplication.java
+++ src/main/java/com/springboot/springbootproject/SpringbootprojectApplication.java
@@ -2,8 +2,10 @@ package com.springboot.springbootproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// To exclude the spring security credentials page in start use below: exclude = SecurityAutoConfiguration.class
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringbootprojectApplication {
 
 	public static void main(String[] args) {
