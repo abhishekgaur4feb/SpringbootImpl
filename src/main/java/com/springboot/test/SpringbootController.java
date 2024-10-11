8abd3488078c6bfdd1a489c9f3ccaa1ace3dc65d
abhishekgaur <abhishekgaur@localhost>
2024-10-11 12:54:41 -0700

    Changes done for controller and entities and repository

--- src/main/java/com/springboot/test/SpringbootController.java
+++ src/main/java/com/springboot/test/SpringbootController.java
@@ -1,4 +1,4 @@
-package com.springboot.springbootproject;
+package com.springboot.test;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/springboot/test/SpringbootprojectApplication.java
+++ src/main/java/com/springboot/test/SpringbootprojectApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.springbootproject;
+package com.springboot.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
