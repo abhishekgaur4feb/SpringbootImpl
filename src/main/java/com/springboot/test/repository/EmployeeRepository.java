02dfa0ec4c82576217013c46a95c4c0828b519e8
abhishekgaur <abhishekgaur@localhost>
2024-10-12 00:03:08 -0700

    Changes for repository

--- src/main/java/com/springboot/test/repository/EmployeeRepository.java
+++ src/main/java/com/springboot/test/repository/EmployeeRepository.java
@@ -1,10 +1,7 @@
 package com.springboot.test.repository;
 
 import org.springframework.data.repository.CrudRepository;
-
 import com.springboot.test.entities.Employee;
-
-
 
 public interface EmployeeRepository extends CrudRepository<Employee, Integer>{
  public Employee findById(int id);
