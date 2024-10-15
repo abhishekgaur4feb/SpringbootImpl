1e291268675f92e70af8e04ebbdee940b7e5db45
abhishekgaur <abhishekgaur@localhost>
2024-10-15 16:28:44 -0700

    Update data in database code added

--- src/main/java/com/springboot/test/SpringbootController.java
+++ src/main/java/com/springboot/test/SpringbootController.java
@@ -3,6 +3,7 @@ package com.springboot.test;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.jasper.tagplugins.jstl.core.Catch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -10,8 +11,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -44,8 +47,16 @@ public class SpringbootController {
 	
 	@PostMapping("/addemployee")
 	public Employee addEmployee(@RequestBody Employee employee) {
-		return employeeService.addEmployee(employee);		
+		return employeeService.addEmployee(employee);	
 	}
+	
+	
+	@PutMapping("/updateemployee/{id}")
+	public Employee updateemployeeinfo(@RequestBody Employee employee,@PathVariable("id") int employeeid) {
+		return employeeService.updateEmployeeDetails(employee,employeeid);	
+	}
+	
+	
 	
 	
 	@DeleteMapping("/deleteemployee/{id}")
--- src/main/java/com/springboot/test/service/EmployeeService.java
+++ src/main/java/com/springboot/test/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.springboot.test.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import com.springboot.test.entities.Employee;
@@ -10,40 +13,51 @@ import com.springboot.test.repository.EmployeeRepository;
 
 @Component
 public class EmployeeService {
-    @Autowired
+	@Autowired
 	private EmployeeRepository employeeRepository;
-	
-	
+
+
 	//Find all employee
 	public List<Employee> getAllEmployee(){
-	return (List<Employee>) this.employeeRepository.findAll(); 
+		return (List<Employee>) this.employeeRepository.findAll(); 
 	}
-	
+
 	//add employee
-		public Employee addEmployee(Employee employee) {
-			 Employee result= employeeRepository.save(employee);
-			 return result;
-		}	
-	
+	public Employee addEmployee(Employee employee) {
+		Employee result= employeeRepository.save(employee);
+		return result;
+	}	
 
-		//Get Employee by id
-		public Employee getEmployeedetailsById(int employeeid) {
-			Employee result= employeeRepository.findById(employeeid);	
-			return result;
-			
-		}
+
+	//Get Employee by id
+	public Employee getEmployeedetailsById(int employeeid) {
+		Employee result= employeeRepository.findById(employeeid);	
+		return result;
+
+	}
+
+	//Delete Employee Information by id
+	public void deletEmployeeById(int employeeid) {
+		employeeRepository.deleteById(employeeid);
+	}
+
+	//delete all entries
+	public void deleteAllEmployee() {
+		employeeRepository.deleteAll();
+
+	}
+	//update employee Information in DB
+	public Employee updateEmployeeDetails(Employee employee, int employeeid) {
+
+		Employee employee_info = employeeRepository.findById(employeeid);
 		
-		//Delete Employee Information by id
-		public void deletEmployeeById(int employeeid) {
-			employeeRepository.deleteById(employeeid);
-		}
-		
-		//delete all entries
-		public void deleteAllEmployee() {
-			employeeRepository.deleteAll();
-			
-		}
+		employee_info.setCity(employee.getCity());
+		employee_info.setName(employee.getName());
+		employee_info.setStatus(employee.getStatus());
+	    employeeRepository.save(employee_info);
+	       return employee_info;
 
 
 
+	}
 }
